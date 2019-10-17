ac8e9bd8f30894b3514eaf84260d5168c1bf2356
Liu, Xiao <lio4072@hotmail.com>
2019-10-17 19:26:16 +0900

    solved prob 14, in a very dirty way...

--- 14_longCommPrefix.py
+++ 14_longCommPrefix.py
@@ -1,8 +1,13 @@
 from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        minStrLen = strs[0]
+        if strs == []:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        minStrLen = len(strs[0])
         minStrNum = 0
+        longest = ""
         i = 0
         for s in strs:
             if len(s) < minStrLen:
@@ -10,11 +15,31 @@ class Solution:
                 minStrNum = i
             i += 1
         minStr = strs[minStrNum]
+        del strs[minStrNum]
+        i = 0
         for c in minStr:
-            
+            tmpEqual = False
+            for l in strs:
+                # print(c+": "+l[i])
+                if c == l[i]:
+                    # print(l[i], end="")
+                    tmpEqual = True
+                else:
+                    tmpEqual = False
+                    break
+            # print()
+            i += 1
+            if tmpEqual == True:
+                longest += c
+            else:
+                break
+
+        return longest
 
 data1 = ["flower","flow","flight"]
 data2 = ["dog","racecar","car"]
+data3 = ["c","acc","ccc"]
 x = Solution()
 print(x.longestCommonPrefix(data1))
 print(x.longestCommonPrefix(data2))
+print(x.longestCommonPrefix(data3))
