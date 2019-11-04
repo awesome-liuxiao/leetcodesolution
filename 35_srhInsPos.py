f4b01fb73e2680f2b572268d42519148dce76796
Liu, Xiao <lio4072@hotmail.com>
2019-11-04 20:45:17 +0900

    no change

--- 35_srhInsPos.py
+++ 35_srhInsPos.py
@@ -7,22 +7,25 @@ class Solution:
         res = nums.index(target)
         # print(res)
         return res
-        
 
 x = Solution()
 
-nums = [1,3,5,6]
-target = 5
-x.searchInsert(nums, target) # 2
+# nums = [1,3,5,6]
+# target = 5
+# x.searchInsert(nums, target) # 2
 
-nums = [1,3,5,6]
+# nums = [1,3,5,6]
+# target = 2
+# x.searchInsert(nums, target) # 1
+
+# nums = [1,3,5,6]
+# target = 7
+# x.searchInsert(nums, target) # 4
+
+# nums = [1,3,5,6]
+# target = 0
+# x.searchInsert(nums, target) # 0
+
+nums = [1,3]
 target = 2
 x.searchInsert(nums, target) # 1
-
-nums = [1,3,5,6]
-target = 7
-x.searchInsert(nums, target) # 4
-
-nums = [1,3,5,6]
-target = 0
-x.searchInsert(nums, target) # 0
