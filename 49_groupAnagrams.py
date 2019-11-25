10ce4ceefffd2bb9ed799b7d6b93c2254a73a784
Liu, Xiao <lio4072@hotmail.com>
2019-11-25 17:41:33 +0900

    wasted few days, solved this refered others

--- 49_groupAnagrams.py
+++ 49_groupAnagrams.py
@@ -1,37 +1,24 @@
 from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sizeDict = {}
-        res = []
+        res = {}
         for word in strs:
-            tmpSize = self.getSize(word)
-            sizeDict[word] = tmpSize
-        tmpList = []
-        for word in strs:
-            i = 0
-            for wordd in sizeDict:
-                if sizeDict[word] == sizeDict[wordd]:
-                    tmpList.append(word)
-            print(tmpList)
-            if tmpList not in res:
-                res.append(tmpList)
-            tmpList = []
-        print(res)
-        return res
+            sortedWord = "".join(sorted(word))
+            if sortedWord in res:
+                res[sortedWord].append(word)
+            else:
+                res[sortedWord] = [word]
+        # print(list(res.values()))
+        return list(res.values())
 
-    def getSize(self, word):
-        size = 0
-        for c in word:
-            size += ord(c)
-        return size
 
 x = Solution()
 
 strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-# x.groupAnagrams(strs)
+x.groupAnagrams(strs)
 
 strs = ["",""]
 x.groupAnagrams(strs)
 
 strs = [""]
-# x.groupAnagrams(strs)
+x.groupAnagrams(strs)
