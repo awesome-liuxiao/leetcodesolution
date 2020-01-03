2f0b2613277aea6073c6ceded11a1398cdf94930
Liu, Xiao <lio4072@hotmail.com>
2020-01-03 17:56:14 +0900

    modified 70

--- 70_climbingStairs.py
+++ 70_climbingStairs.py
@@ -5,7 +5,7 @@ class Solution:
         steps = [1,1]
         for i in range(2,n+1):
             steps.append(steps[i-1]+steps[i-2])
-        print(steps)
+        # print(steps)
         return steps[n]
         
 x = Solution()
