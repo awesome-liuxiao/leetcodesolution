8ae8c56ad35b75de7755a6f2494f5035bbb0d717
Liu, Xiao <lio4072@hotmail.com>
2020-05-27 17:25:41 +0900

    solved 78. referred

--- 78_subsets.py
+++ 78_subsets.py
@@ -2,7 +2,7 @@ from typing import List
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        def helper(index, subset):
+        def helper(index, subset): #backtracking
             res.append(subset)
             for i in range(index, len(nums)):
                 helper(i+1, subset+[nums[i]])
