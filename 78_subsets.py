6d9fc228f7ed7a71d99110779f6beb67a643bff2
Liu, Xiao <lio4072@hotmail.com>
2020-05-27 17:25:06 +0900

    solved 78

---
+++ 78_subsets.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def helper(index, subset):
+            res.append(subset)
+            for i in range(index, len(nums)):
+                helper(i+1, subset+[nums[i]])
+        helper(0, [])
+        print(res)
+        return res
+
+
+X = Solution()
+
+nums = [1,2,3]
+X.subsets(nums)
