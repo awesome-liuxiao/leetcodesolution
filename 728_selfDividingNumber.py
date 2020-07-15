0bf262eff0aebdd335849371f622b98b3d435ef7
Liu, Xiao <lio4072@hotmail.com>
2020-07-15 14:37:30 +0900

    solved 728

---
+++ 728_selfDividingNumber.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        res = []
+        for n in range(left, right+1):
+            nStr = str(n)
+            if n == 0 or '0' in nStr:
+                continue
+            isDividable = False
+            for c in nStr:
+                if n % int(c) == 0:
+                    isDividable = True
+                else:
+                    isDividable = False
+                    break
+            if isDividable:
+                res.append(n)
+        return res
+
+X = Solution()
+
+left = 1
+right = 22
+print(X.selfDividingNumbers(left, right))
